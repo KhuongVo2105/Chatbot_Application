f3f985d9a8fc6787fd3dcf024ece2d9a0f9d1698
longpl-1902 <lephilong1902@gmail.com>
2025-08-23 00:39:08 +0700

    update send message command

--- backend/app/routes/message.py
+++ backend/app/routes/message.py
@@ -19,12 +19,19 @@ route = APIRouter(
 HUGGING_FACE_API_KEY = settings.HUGGING_FACE_API_KEY
 HUGGING_FACE_MODEL_ID = settings.HUGGING_FACE_MODEL_ID
 
-print("HUGGING_FACE_API_KEY: ", HUGGING_FACE_API_KEY, " - ", "HUGGING_FACE_MODEL_ID: ", HUGGING_FACE_MODEL_ID)
+print("HUGGING_FACE_API_KEY: ", HUGGING_FACE_API_KEY, " - ",
+      "HUGGING_FACE_MODEL_ID: ", HUGGING_FACE_MODEL_ID)
 
 client = InferenceClient(
     model=HUGGING_FACE_MODEL_ID,
     token=HUGGING_FACE_API_KEY
 )
+
+raw_client = InferenceClient(
+    model=HUGGING_FACE_MODEL_ID,
+    token=HUGGING_FACE_API_KEY
+)
+
 
 @route.get("/conversation/{conversation_id}/ids", response_model=Response[List[PydanticObjectId]])
 async def get_message_ids_in_conversation(conversation_id: PydanticObjectId, current_user: User = Depends(get_current_user)):
@@ -33,11 +40,13 @@ async def get_message_ids_in_conversation(conversation_id: PydanticObjectId, cur
     """
     conversation = await Conversation.find_one(Conversation.id == conversation_id, Conversation.user.id == current_user.id)
     if not conversation:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Conversation not found or you don't have permission")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Conversation not found or you don't have permission")
 
     messages = await Message.find(Message.conversation.id == conversation_id).project(Message).to_list()
     message_ids = [msg.id for msg in messages]
     return Response(data=message_ids)
+
 
 @route.get("/conversation/{conversation_id}", response_model=Response[List[MessageOut]])
 async def get_messages_in_conversation(conversation_id: PydanticObjectId, skip: int = 0, limit: int = 100, current_user: User = Depends(get_current_user)):
@@ -46,10 +55,12 @@ async def get_messages_in_conversation(conversation_id: PydanticObjectId, skip: 
     """
     conversation = await Conversation.find_one(Conversation.id == conversation_id, Conversation.user.id == current_user.id)
     if not conversation:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Conversation not found or you don't have permission")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Conversation not found or you don't have permission")
 
     messages = await Message.find(Message.conversation.id == conversation_id).sort(+Message.created_at).skip(skip).limit(limit).to_list()
     return Response(data=messages)
+
 
 @route.get("/{message_id}", response_model=Response[MessageOut])
 async def get_message(message_id: PydanticObjectId, current_user: User = Depends(get_current_user)):
@@ -58,24 +69,29 @@ async def get_message(message_id: PydanticObjectId, current_user: User = Depends
     """
     message = await Message.get(message_id, fetch_links=True)
     if not message:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Message not found")
-    
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Message not found")
+
     if message.conversation.user.id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You don't have permission to access this message")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="You don't have permission to access this message")
 
     return Response(data=message)
 
+
 @route.post("/", status_code=status.HTTP_201_CREATED, response_model=Response[MessageOut])
-async def create_message(message_data: MessageCreate, current_user: User = Depends(get_current_user)):
+async def create_message_with_rag(message_data: MessageCreate, current_user: User = Depends(get_current_user)):
     """
     Create a new message in a conversation.
     """
     global rag
     conversation = await Conversation.find_one(Conversation.id == message_data.conversation_id, Conversation.user.id == current_user.id)
     if not conversation:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Conversation not found or you don't have permission")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Conversation not found or you don't have permission")
 
-    new_user_message = Message(**message_data.model_dump(exclude={"conversation_id"}), conversation=conversation.id, sender_type="User")
+    new_user_message = Message(**message_data.model_dump(
+        exclude={"conversation_id"}), conversation=conversation.id, sender_type="User")
     await new_user_message.insert()
 
     rag_context = rag_instance.generate_prompt(message_data.content)
@@ -90,14 +106,73 @@ async def create_message(message_data: MessageCreate, current_user: User = Depen
         chatbot_response_content = completion.choices[0].message.content
     except Exception as e:
         # Xử lý lỗi nếu việc gọi API Hugging Face thất bại
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Failed to get response from chatbot: {e}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"Failed to get response from chatbot: {e}")
 
-    new_bot_message = Message(conversation=conversation.id, sender_type="Bot", content=chatbot_response_content)
+    new_bot_message = Message(conversation=conversation.id,
+                              sender_type="Bot", content=chatbot_response_content)
     await new_bot_message.insert()
 
     await conversation.update_last_updated()
 
     return Response(status_code=status.HTTP_201_CREATED, message="Message created successfully with bot response", data=new_bot_message)
+
+
+@route.post("/raw/", status_code=status.HTTP_201_CREATED, response_model=Response[MessageOut])
+async def create_raw_message(message_data: MessageCreate, current_user: User = Depends(get_current_user)):
+    """ Create a new raw message in a conversation without RAG processing.
+    """
+    conversation = await Conversation.find_one(Conversation.id == message_data.conversation_id, Conversation.user.id == current_user.id)
+    if not conversation:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Conversation not found or you don't have permission")
+
+    try:
+        completion = client.chat.completions.create(
+            model=client.model,
+            messages=[
+                {"role": "user", "content": message_data.content}
+            ],
+        )
+
+        chatbot_response_content = completion.choices[0].message.content
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"Failed to get response from chatbot: {e}")
+
+    new_bot_message = Message(conversation=conversation.id,
+                              sender_type="Bot", content=chatbot_response_content)
+    await new_bot_message.insert()
+    await conversation.update_last_updated()
+    return Response(status_code=status.HTTP_201_CREATED, message="Raw message created successfully with bot response", data=new_bot_message)
+
+
+@route.post("raw-model/", status_code=status.HTTP_201_CREATED, response_model=Response[MessageOut])
+async def create_raw_message_with_model(message_data: MessageCreate, current_user: User = Depends):
+    conversation = await Conversation.find_one(Conversation.id == message_data.conversation_id, Conversation.user.id == current_user.id)
+    if not conversation:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Conversation not found or you don't have permission")
+
+    try:
+        completion = raw_client.chat.completions.create(
+            model=raw_client.model,
+            messages=[
+                {"role": "user", "content": message_data.content}
+            ],
+        )
+
+        chatbot_response_content = completion.choices[0].message.content
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"Failed to get response from chatbot: {e}")
+
+    new_bot_message = Message(conversation=conversation.id,
+                              sender_type="Bot", content=chatbot_response_content)
+    await new_bot_message.insert()
+    await conversation.update_last_updated()
+    return Response(status_code=status.HTTP_201_CREATED, message="Raw message created successfully with raw bot response", data=new_bot_message)
+
 
 @route.delete("/{message_id}", response_model=Response)
 async def delete_message(message_id: PydanticObjectId, current_user: User = Depends(get_current_user)):
@@ -106,11 +181,12 @@ async def delete_message(message_id: PydanticObjectId, current_user: User = Depe
     """
     message = await Message.get(message_id, fetch_links=True)
     if not message:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Message not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Message not found")
 
     if message.conversation.user.id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You don't have permission to delete this message")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="You don't have permission to delete this message")
 
     await message.delete()
     return Response(message="Message deleted successfully")
-
